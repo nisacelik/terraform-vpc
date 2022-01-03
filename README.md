befa42dbe0eb908f337aaf57d5737a223668f8d5
nisaclk <niisacelik@gmail.com>
2022-01-04 00:51:08 +0300

    created VPC

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # terraform-vpc
+
+**Virtual Private Cloud (VPC) with Public and Private Subnets, Internet Gateway, and Route Tables using Terraform**
---
+++ variable.tf
@@ -0,0 +1,36 @@
+# VPC Variables
+variable "region" {
+  default       = "eu-central-1"
+  description   = "AWS Region"
+  type          = string
+}
+
+variable "vpc-cidr" {
+  default       = "10.0.0.0/16"
+  description   = "VPC CIDR Block"
+  type          = string
+}
+
+variable "public-subnet-1-cidr" {
+  default       = "10.0.0.0/24"
+  description   = "Public Subnet 1 CIDR Block"
+  type          = string
+}
+
+variable "public-subnet-2-cidr" {
+  default       = "10.0.1.0/24"
+  description   = "Public Subnet 2 CIDR Block"
+  type          = string
+}
+
+variable "private-subnet-1-cidr" {
+  default       = "10.0.2.0/24"
+  description   = "Private Subnet 1 CIDR Block"
+  type          = string
+}
+
+variable "private-subnet-2-cidr" {
+  default       = "10.0.3.0/24"
+  description   = "Private Subnet 2 CIDR Block"
+  type          = string
+}
